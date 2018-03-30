b4f71241c97d54c1502b91dd2b1a925a83c9900f
LizPeng <lizhipeng2013@163.com>
2018-03-30 11:47:34 +0800

    add formatNumber util

---
+++ util.js
@@ -0,0 +1,33 @@
+// Extend the default Number object with a formatMoney() method:
+// usage: someVar.formatMoney(decimalPlaces, symbol, thousandsSeparator, decimalSeparator)
+// defaults: (2, "$", ",", ".")
+Number.prototype.formatMoney = function (places, symbol, thousand, decimal) {
+  places = !isNaN(places = Math.abs(places)) ? places : 2;
+  symbol = symbol !== undefined ? symbol : "$";
+  thousand = thousand || ",";
+  decimal = decimal || ".";
+  var number = this,
+    negative = number < 0 ? "-" : "",
+    i = parseInt(number = Math.abs(+number || 0).toFixed(places), 10) + "",
+    j = (j = i.length) > 3 ? j % 3 : 0;
+  return symbol + negative + (j ? i.substr(0, j) + thousand : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousand) + (places ? decimal + Math.abs(number - i).toFixed(places).slice(2) : "");
+};
+// To set it up as a global function:
+function formatMoney(number, places, symbol, thousand, decimal) {
+  if(isNaN(+number)){
+    console.error('+number类型必须为数字')
+    return 
+  }
+  number = +number || 0;
+  places = !isNaN(places = Math.abs(places)) ? places : 2;
+  // symbol = symbol !== undefined ? symbol : "$";
+  thousand = thousand || ",";
+  decimal = decimal || ".";
+  var negative = number < 0 ? "-" : "",
+    i = parseInt(number = Math.abs(+number).toFixed(places)) + "", //+number把字符串转换为数字
+    j = (j = i.length) > 3 ? j % 3 : 0;
+  return symbol + negative + 
+        (j ? (i.substr(0, j) + thousand) : "") +  
+        i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousand) + 
+        (places ? decimal + Math.abs(number - i).toFixed(places).slice(2) : "");
+}
