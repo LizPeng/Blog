2759a48f1e624f0f81fefe0a04489adf8e22a312
LizPeng <lizhipeng2013@163.com>
2018-06-26 10:31:18 +0800

    基础类型完成

--- TS/variableDeclarations.ts
+++ TS/variableDeclarations.ts
@@ -13,3 +13,24 @@ let c: Color = Color.Green;
 function warnUser(): void{
     alert("blabla")
 }
+// null 和 undefined
+// 是所有类型的子类型
+
+/**
+ * Never
+ * 表示是那些永不存在的值的类型
+ * 是那些会抛出异常或根本就不会有返回值的函数表达式或箭头函数表达式的返回值类型
+ * 
+ */
+function error(message: string): never {
+    throw new Error(message);
+}
+
+// 断言类型 ，两种形式
+// 尖括号语法，as语法
+let someValue: any = "this is a string";
+
+let strLength: number = (<string>someValue).length
+let strasLength: number = (someValue as string).length
+
+// 当你在ts使用jsx时，只有as语法是被允许的
